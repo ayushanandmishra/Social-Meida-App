1d26fd49d0667b1ac89b29322c571aaeff0546e6
ayush anand <ayushanandmishra@gmail.com>
2023-06-21 23:46:33 +0530

    login and signup page change

--- client/src/Components/SignUpPage/SignupForm/SignupForm.jsx
+++ client/src/Components/SignUpPage/SignupForm/SignupForm.jsx
@@ -27,7 +27,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Social Group
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
